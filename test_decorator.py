8cac524ef2890c5af64bd570f04d84b9735e41b0
renzon <renzon@gmail.com>
2017-09-28 11:18:42 -0300

    Test with decorator lib. On Python 2 pytest DI will not work on regular decorators

--- test_decorator.py
+++ test_decorator.py
@@ -1,6 +1,7 @@
 from functools import wraps
 
 import pytest
+from decorator import decorator
 
 
 def regular_decorator(foo, bar):
@@ -26,9 +27,28 @@ def b():
     return 'b'
 
 
-@pytest.mark.parametrize(
+cd_parametrize = pytest.mark.parametrize(
     'c,d', [('c', 'd'), ('C', 'D')], ids='low upper'.split())
+
+
+@cd_parametrize
 @regular_decorator('foo', 'bar')
 def test_regular_decorator(a, b, c, d):
+    assert ('a', 'b', 'c'.capitalize(), 'd'.capitalize()) == (
+        a, b, c.capitalize(), d.capitalize())
+
+
+def lib_decorator(foo, bar):
+    @decorator
+    def wrapper(func, *args, **kwargs):
+        print(foo, bar)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@cd_parametrize
+@lib_decorator('foo', 'bar')
+def test_lib_decorator(a, b, c, d):
     assert ('a', 'b', 'c'.capitalize(), 'd'.capitalize()) == (
         a, b, c.capitalize(), d.capitalize())
